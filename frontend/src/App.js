fa0fb2e3daddbbfad6cdc9cf9d6e0484b2c59d5b
€mily <22375770+emilywritescode@users.noreply.github.com>
2020-08-28 23:43:41 -0400

    initial design templating (React)

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import MainApp from './MainApp.js';
+
+function App() {
+    return(ReactDOM.render(<MainApp />, document.getElementById('root')));
+}
+
+export default App;
