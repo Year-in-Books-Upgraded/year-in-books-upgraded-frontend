a97c26b759e626ce2f67c951a539e4408358ad00
€mily <22375770+emilywritescode@users.noreply.github.com>
2020-09-08 22:22:07 -0400

    adding beautifulsoup for XML parsing

--- backend/server.py
+++ backend/server.py
@@ -4,70 +4,157 @@ import oauth2 as oauth
 from dotenv import load_dotenv
 load_dotenv()
 import os
+from bs4 import BeautifulSoup as soup
+from datetime import date
 
-url = 'https://www.goodreads.com'
-# request_token_url = '%s/oauth/request_token' % url
-# authorize_url = '%s/oauth/authorize' % url
-# access_token_url = '%s/oauth/access_token' % url
 
-> Create client to ping Goodreads API (using dev key + secret)
-consumer = oauth.Consumer(key=os.getenv('GR_CLIENT_KEY'), secret=os.getenv('GR_CLIENT_SECRET'))
-# client = oauth.Client(consumer)
+class Server:
 
-# > Ask for a request token
-# response, content = client.request(request_token_url, 'GET')
-# if response['status'] != '200':
-#     raise Exception('Invalid response: %s, content: ' % response['status'] + content)
-#
-# request_token_result = dict(urllib.parse.parse_qsl(content))
-# print(request_token_result)
-#
-# > Decode bytes into strings
-# request_token_decoded = {}
-# for key, value in request_token_result.items():
-#     request_token_decoded[key.decode('utf-8')] = value.decode('utf-8')
-# print(request_token_decoded)
-# request_token = request_token_decoded['oauth_token']
-# request_token_secret = request_token_decoded['oauth_token_secret']
-#
-# > Get authorize link for user to use
-# authorize_link = '%s?oauth_token=%s' % (authorize_url, request_token)  # uses request token
-# print('Use a browser to visit this link and accept your application:')
-# print(authorize_link)
-#
-# Wait for user to visit authorize link and accept
-# accepted = 'n'
-# while accepted.lower() == 'n':
-#     # you need to access the authorize_link via a browser,
-#     # and proceed to manually authorize the consumer
-#     accepted = input('Have you authorized me? (y/n) ')
-#
-#
-# > Now we need to get access token!
-# token = oauth.Token(request_token, request_token_secret)
-#
-# client = oauth.Client(consumer, token)
-# response, content = client.request(access_token_url, 'POST')
-# if response['status'] != '200':
-#     raise Exception('Invalid response: %s' % response['status'])
-#
-# access_token = dict(urllib.parse.parse_qsl(content))
-# print(access_token)
-#
-# access_token_decoded = {}
-# for key, value in access_token.items():
-#     access_token_decoded[key.decode('utf-8')] = value.decode('utf-8')
-# print(access_token_decoded)
-# access_token = access_token_decoded['oauth_token']
-# access_secret = access_token_decoded['oauth_token_secret']
-access_token = os.getenv('GR_ACCESS_KEY']
-access_secret = os.getenv('GR_ACCESS_SECRET']
+    def __init__(self):
+        self.goodreads_url = 'https://www.goodreads.com'
+        self.request_token_url = '%s/oauth/request_token' % self.goodreads_url
+        self.authorize_url = '%s/oauth/authorize' % self.goodreads_url
+        self.access_token_url = '%s/oauth/access_token' % self.goodreads_url
+        self.client = None
 
-# > Save token for future use
-# print('Save this for later: ')
-# print('oauth token key:    ' + access_token_decoded['oauth_token'])
-# print('oauth token secret: ' + access_token_decoded['oauth_token_secret'])
-token = oauth.Token(access_token, access_token_secret)
+    # ** AUTHENTICATION (to get user access key) **
+    def authenticate_user(self):
+        # > Create client to ping Goodreads API (using dev key + secret)
+        consumer = oauth.Consumer(key=os.getenv('GR_CLIENT_KEY'), secret=os.getenv('GR_CLIENT_SECRET'))
+        # client = oauth.Client(consumer)
 
-client = oauth.Client(consumer, token)
-# Ask for user ID from Goodreads API (/api/auth_user)
+        # > Ask for a request token
+        # response, content = client.request(request_token_url, 'GET')
+        # if response['status'] != '200':
+        #     raise Exception('Invalid response: %s, content: ' % response['status'] + content)
+        #
+        # request_token_result = dict(urllib.parse.parse_qsl(content))
+        # print(request_token_result)
+        #
+        # > Decode bytes into strings (for request token)
+        # request_token_decoded = {}
+        # for key, value in request_token_result.items():
+        #     request_token_decoded[key.decode('utf-8')] = value.decode('utf-8')
+        # print(request_token_decoded)
+        # request_token = request_token_decoded['oauth_token']
+        # request_token_secret = request_token_decoded['oauth_token_secret']
+        #
+        # > Get authorize link for user to use
+        # authorize_link = '%s?oauth_token=%s' % (authorize_url, request_token)  # uses request token
+        # print('Use a browser to visit this link and accept your application:')
+        # print(authorize_link)
+        #
+        # Wait for user to visit authorize link and accept
+        # accepted = 'n'
+        # while accepted.lower() == 'n':
+        #     # you need to access the authorize_link via a browser,
+        #     # and proceed to manually authorize the consumer
+        #     accepted = input('Have you authorized me? (y/n) ')
+        #
+        #
+        # > Now we need to get access token!
+        # token = oauth.Token(request_token, request_token_secret)  # request token, will get access token from this
+        #
+        # client = oauth.Client(consumer, token)  # client still using request token
+        # response, content = client.request(access_token_url, 'POST')
+        # if response['status'] != '200':
+        #     raise Exception('Invalid response: %s' % response['status'])
+        #
+        # access_token = dict(urllib.parse.parse_qsl(content))
+        # print(access_token)
+        #
+        # > Decode bytes into strings (for access token)
+        # access_token_decoded = {}
+        # for key, value in access_token.items():
+        #     access_token_decoded[key.decode('utf-8')] = value.decode('utf-8')
+        # print(access_token_decoded)
+        # access_token = access_token_decoded['oauth_token']
+        # access_secret = access_token_decoded['oauth_token_secret']
+        access_token = os.getenv('GR_ACCESS_TOKEN')
+        access_secret = os.getenv('GR_ACCESS_SECRET')
+
+        # > Save token for future use
+        # print('Save this for later: ')
+        # print('oauth token key:    ' + access_token_decoded['oauth_token'])
+        # print('oauth token secret: ' + access_token_decoded['oauth_token_secret'])
+        token = oauth.Token(access_token, access_secret) # this has the access token for the user
+
+        self.client = oauth.Client(consumer, token)  # client that has the access token!
+
+
+    # ** GOODREADS API METHODS **
+
+    # Ask for user ID (GET /api/auth_user)
+    def goodreads_get_user_id(self):
+        response, content = self.client.request('%s/api/auth_user' % self.goodreads_url,'GET')
+        if response['status'] != '200':
+           raise Exception('Cannot fetch resource: %s' % response['status'])
+        user_xml = soup(content, 'xml')
+        user_id = user_xml.user['id']
+        user_name = user_xml.find('name').contents[0]
+        # print(user_id)
+        # print(user_name)
+        return user_id, user_name
+
+    # Get info about user (GET /user/show, params = (id))
+    def goodreads_get_user_info(self, user_id):
+        response, content = self.client.request('%s/user/show/%s.xml' % (self.goodreads_url, user_id), 'GET')
+        if response['status'] != '200':
+           raise Exception('Cannot fetch resource: %s' % response['status'])
+        # print(content)
+        user_xml = soup(content, 'xml')
+        year_joined = user_xml.joined.contents[0].split('/')[1]
+        profile_image = user_xml.image_url.contents[0]
+        profile_url = user_xml.link.contents[0]
+        # print(year_joined)
+        # print(profile_image)
+        # print(profile_url)
+        return (year_joined, profile_image, profile_url)
+
+    # Get books on user's read shelf (GET /review/list.xml?v=2, params = (shelf, sort, read_at))
+    def goodreads_get_read_shelf(self, year):
+        response, content = self.client.request('%s/review/list.xml?v=2&shelf=read&sort=date_read&read_at=%s&per_page=200' % (self.goodreads_url, year), 'GET')
+        if response['status'] != '200':
+           raise Exception('Cannot fetch resource: %s' % response['status'])
+        #print(content)
+        return (content)
+
+
+    def get_year_data(self, year, xml_content):
+        print(f'==={year}====')
+        reviews_xml = soup(xml_content, 'xml')
+        total_books = reviews_xml.reviews['total']
+        all_reviews_xml = reviews_xml.reviews.find_all('review')
+
+        reviews = []
+        for review_xml in all_reviews_xml:
+            review = {
+                'title' : review_xml.title_without_series.contents[0],
+                'author' : [author.find('name').contents[0] for author in review_xml.authors.findAll('author')],
+                'gr_link' : review_xml.link.contents[0],
+                'num_pages' : review_xml.num_pages.contents[0] if review_xml.num_pages.contents else 0,
+                'num_reads' : review_xml.ratings_count.contents[0],
+                'avg_rating' : review_xml.average_rating.contents[0],
+                'cover' : review_xml.image_url.contents[0],
+                'your_rating' : review_xml.rating.contents[0]
+            }
+            reviews.append(review)
+
+        return (total_books, reviews)
+
+
+def main():
+    server = Server()  # initialize server
+    server.authenticate_user()  # establishes client with user access
+    user_id, user_name = server.goodreads_get_user_id()  # get user's Goodreads ID for use in some API queries
+    year_joined, profile_image, profile_url = server.goodreads_get_user_info(user_id)
+    current_year = date.today().year
+    all_years = list(range(int(year_joined), current_year+1)) # years user can choose from sidebar menu
+    for year in all_years:
+        content = server.goodreads_get_read_shelf(year)
+        year_data = server.get_year_data(year, content)
+        print(f'{year}\n\n{year_data}')
+
+
+if __name__ == '__main__':
+    main()
