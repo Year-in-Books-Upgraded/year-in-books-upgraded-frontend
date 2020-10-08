ef09b3973f4345ca7626636dbf4d8736f6e965e0
€mily <22375770+emilywritescode@users.noreply.github.com>
2020-10-08 13:39:03 -0400

    error handling + sass styling

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 import goodreads_logo from '../images/goodreads_logo.png';
 
-import { LoadingPage } from './Handling.js'
+import { LoadingPage, ErrorPage } from './Handling.js'
 
 class LandingPage extends Component {
 
@@ -13,7 +13,9 @@ class LandingPage extends Component {
         this.state = {
             user_id : '',
             loading : false,
-            data_fetched : false
+            data_fetched : false,
+            error : false,
+            error_type : 0
         };
         this.handleUserID = this.handleUserID.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -32,16 +34,39 @@ class LandingPage extends Component {
                 this.setState({loading : false});
                 this.setState({data_fetched : true});
             })
+            .catch(error => {
+                setTimeout(() => {
+                    this.setState({loading : false});
+                    this.setState({error : true});
+                    var error_msg = error.response.data;
+                    if(error_msg.includes('read book')) {
+                        this.setState({error_type : 2});
+                    } else if(error_msg.includes('invalid')) {
+                        this.setState({error_type : 1});
+                    } else {
+                        this.setState({error_type : 3});
+                    }
+                }, 2000);
+
+            });
         event.preventDefault();
     }
 
     render() {
         if(this.state.loading) {
-            return <LoadingPage/>
+            return <LoadingPage />
         } else if(this.state.data_fetched) {
             let all_years_array = JSON.parse(sessionStorage.getItem('user_data')).all_years
             let latest_year = all_years_array[all_years_array.length - 1]
             return <Redirect to={'/year/'+latest_year.toString()} />
+        } else if(this.state.error) {
+            if(this.state.error_type === 1){
+                return <ErrorPage error_message="Unable to retrieve user data. You may have entered an invalid user ID or this Goodreads user may limit their profile settings." />
+            } else if(this.state.error_type === 2){
+                return <ErrorPage error_message="This user has no read books." />
+            } else {
+                return <ErrorPage error_message="The application encountered an error." />
+            }
         }
         return(
             <div className="full-page-wrapper">
